7317a0f3f2859cedccbb5c63b31bddfcdebbcdab
Mike Larsson <mikelarssonftw@gmail.com>
2013-08-19 14:45:34 -0400

    Remove unnecessary test util method

--- test/utils.js
+++ test/utils.js
@@ -5,13 +5,3 @@ exports.buildApp = function buildApp(options) {
   var app =  obvs.app.build(options);
   return app;
 };
-
-exports.buildServer = function buildServer(options) {
-  var app = exports.buildApp(options);
-  var server = app.listen(0, function(){ 
-    this.emit('ready'); 
-  });
-  server.on('ready', function(){
-    console.log('woop woop');
-  });
-};
